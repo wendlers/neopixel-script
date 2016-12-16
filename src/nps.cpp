b48ba75689836401c1feb75dfc867ab2557250a6
Stefan Wendler <sw@kaltpost.de>
2016-12-16 22:48:10 +0100

    Annies branch

--- src/nps.cpp
+++ src/nps.cpp
@@ -8,7 +8,19 @@ LedFX fx = LedFX();
 
 void nps_main(void)
 {
-  fx.putString("*** Hello NeoPixel ***", CRGB::Blue);
+  fx.setBrightness(150);
+  fx.setPixel((uint16_t)0, CRGB::Red);
+  fx.show(250);
+  fx.setPixel((uint16_t)1, CRGB::Red);
+  fx.setPixel((uint16_t)0, CRGB::Black);
+  fx.show(250);
+  fx.setPixel((uint16_t)2, CRGB::Red);
+  fx.setPixel((uint16_t)1, CRGB::Black);
+  fx.show(250);
+  fx.setPixel((uint16_t)3, CRGB::Red);
+  fx.setPixel((uint16_t)2, CRGB::Black);
+  fx.show(250);
+  fx.setPixel((uint16_t)3, CRGB::Black);
 
   fx.show();
 }
